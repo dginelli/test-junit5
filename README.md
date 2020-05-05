ea0a5256cc6dd00f1249a63fd69e3433dded2ccd
repairnator <noreply@github.com>
2020-05-05 16:04:05 +0200

    Bug commit from dginelli-test-junit This commit is based on the source code from the following commit: dginelli-test-junit-ctco-dev-my-goal-list-421476556-20180828-112001-fix-pom

---
+++ README.md
@@ -0,0 +1,5 @@
+# my_goal_list [![Build Status](https://travis-ci.org/ctco-dev/my-goal-list.svg?branch=master)](https://travis-ci.org/ctco-dev/my-goal-list)
+
+To run: `mvn wildfly:run`
+
+
---
+++ src/main/java/lv/ctco/javaschool/goal/entity/TagDto.java
@@ -0,0 +1,32 @@
+package lv.ctco.javaschool.goal.entity;
+
+
+public class TagDto {
+    private String tagMessage;
+    private Long    cnt;
+
+    public TagDto(){
+        this("", 0L);
+    }
+
+    public TagDto(String tagMsg, Long cnt ){
+        this.tagMessage = tagMsg;
+        this.cnt = cnt;
+    }
+
+    public void setTagMessage(String tagMessage) {
+        this.tagMessage = tagMessage;
+    }
+
+    public String getTagMessage() {
+        return tagMessage;
+    }
+
+    public Long getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(Long cnt) {
+        this.cnt = cnt;
+    }
+}
